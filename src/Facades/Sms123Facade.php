7206a441edd1c64dd56f5e3d9ba3294cb8c99ae4
Casper Ho <casperyaoz@gmail.com>
2021-02-25 15:55:46 +0800

    Provide PHP doc for Sms123Facade

--- src/Facades/Sms123Facade.php
+++ src/Facades/Sms123Facade.php
@@ -4,6 +4,16 @@ namespace Cyaoz94\Sms123\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * Class Sms123Facade
+ * @package Cyaoz94\Sms123\Facades
+ *
+ * @method static void sendSms(string $contactNumber, string|null $messageContent = '', string|null $reference_id = null)
+ * @method static void addTemplate(string $templateTitle, string|null $messageContent = '', string|null $reference_id = null)
+ * @method static mixed void getBalance()
+ *
+ * @see \Cyaoz94\Sms123\Sms123
+ */
 class Sms123Facade extends Facade
 {
     /**
